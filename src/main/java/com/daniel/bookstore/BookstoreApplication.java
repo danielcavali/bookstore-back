a8371bec5639ba8c5888a793446aaa905c97de0d
danielcavali <danielcavali.m@gmail.com>
2022-08-04 23:50:57 -0300

    criando a instancia do banco h2 e salvando dados

--- src/main/java/com/daniel/bookstore/BookstoreApplication.java
+++ src/main/java/com/daniel/bookstore/BookstoreApplication.java
@@ -1,13 +1,45 @@
 package com.daniel.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.daniel.bookstore.domain.Categoria;
+import com.daniel.bookstore.domain.Livro;
+import com.daniel.bookstore.repositories.CategoriaRepository;
+import com.daniel.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "info", "livro ti");
+		
+		Livro l1 = new Livro(null, "hardware completo", "desconhecido", " descriçao aqui mas nao tem agora ", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }

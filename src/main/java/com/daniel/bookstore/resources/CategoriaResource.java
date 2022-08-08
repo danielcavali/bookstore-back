4198fadf4c4ee422772aa4f779da8ec09e676e63
danielcavali <danielcavali.m@gmail.com>
2022-08-07 23:47:25 -0300

    criado o primeiro end-point para retornar a categoria e os livros dessa categoria por id

---
+++ src/main/java/com/daniel/bookstore/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.daniel.bookstore.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.bookstore.domain.Categoria;
+import com.daniel.bookstore.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findByID(@PathVariable Integer id) {
+
+		Categoria obj = service.findByID(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
---
+++ src/main/java/com/daniel/bookstore/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.daniel.bookstore.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.bookstore.domain.Categoria;
+import com.daniel.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	
+	@Autowired
+		private CategoriaRepository repository;
+	
+	public Categoria findByID(Integer id) {
+		Optional<Categoria> obj=repository.findById(id);
+		return obj.orElse(null); 
+		
+	}
+}

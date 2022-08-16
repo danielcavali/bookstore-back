dc0e1e3074f1c8a34a803e2fa315ac35674a79e8
danielcavali <danielcavali.m@gmail.com>
2022-08-16 20:22:32 -0300

    erro ao listarlivrosdto

--- src/main/java/com/daniel/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/daniel/bookstore/repositories/LivroRepository.java
@@ -1,12 +1,19 @@
 package com.daniel.bookstore.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.daniel.bookstore.domain.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
+	
+	@Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")
+	List<Livro> findAllByCaegoria(@Param(value = "id_cat ")	Integer id_cat);
 	
 
 }
--- src/main/java/com/daniel/bookstore/services/LivroService.java
+++ src/main/java/com/daniel/bookstore/services/LivroService.java
@@ -1,5 +1,6 @@
 package com.daniel.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,17 @@ public class LivroService {
 	
 	@Autowired
 	private LivroRepository repository;
+	
+	@Autowired
+	private CategoriaService categoriaservice;
+	
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExeption("objeto nao encontrado" + id + "tipo: "+ Livro.class.getName()));
+	}
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaservice.findByID(id_cat);
+		return repository.findAllByCaegoria(id_cat);
 	}
 }

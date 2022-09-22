2c1d6f445502a0bb60f25a2460c1f8cb388cba88
danielcavali <danielcavali.m@gmail.com>
2022-09-22 13:37:32 -0300

    metodo post create para livros

--- src/main/java/com/daniel/bookstore/services/LivroService.java
+++ src/main/java/com/daniel/bookstore/services/LivroService.java
@@ -6,40 +6,51 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.daniel.bookstore.domain.Categoria;
 import com.daniel.bookstore.domain.Livro;
 import com.daniel.bookstore.repositories.LivroRepository;
 import com.daniel.bookstore.services.exeptions.ObjectNotFoundExeption;
 
 @Service
 public class LivroService {
-	
-	
+
 	@Autowired
 	private LivroRepository repository;
-	
+
 	@Autowired
 	private CategoriaService categoriaservice;
-	
-	
+
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundExeption("objeto nao encontrado" + id + "tipo: "+ Livro.class.getName()));
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundExeption("objeto nao encontrado" + id + "tipo: " + Livro.class.getName()));
 	}
+
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaservice.findByID(id_cat);
 		return repository.findAllByCategoria(id_cat);
 	}
+
 	public Livro update(Integer id, Livro obj) {
 		Livro newObj = findById(id);
-		updateData(newObj,obj);
-		
+		updateData(newObj, obj);
+
 		return repository.save(newObj);
-		
+
 	}
+
 	private void updateData(Livro newObj, Livro obj) {
 		// TODO Auto-generated method stub
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaservice.findByID(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
+
 	}
 }
